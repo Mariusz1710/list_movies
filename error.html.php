1b11a8b31084dbb3a7d01ead859cdf9dce87810e
Mariusz <mbielicki@wp.pl>
2023-05-01 15:11:16 +0200

    Add fetching and displaying movies

---
+++ error.html.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>List of movies</title>
+
+</head>
+<body>
+
+<?php
+
+    echo $error;
+
+?>
+
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,9 +3,46 @@
 try
 {
     $pdo = new PDO('mysql:host=localhost;dbname=list_movies','list_movies','ola');
-    echo "OK";
+    $pdo -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $pdo -> exec('SET names UTF8');
 }
 catch(PDOException $e)
 {
-    echo "The error ".$e -> getMessage();
+    $error = 'The error during the connection to the database '.$e -> getMessage();
+    include 'error.html.php';
+    exit();
+}
+
+try
+{
+    $sql = "SELECT title,firstName,lastName,year FROM movies INNER JOIN directors ON movies.directorId = directors.id";
+    $result = $pdo -> query($sql);
+
+    echo <<<END
+
+<table>
+        <tr>
+            <th>No.</th>
+            <th>Title</th>
+            <th>Director</th>
+        </tr>
+
+END;
+
+    $i = 1;
+
+    foreach($result as $row)
+    {
+        echo '<tr><td>'.$i.'</td><td>'.$row['title'].'</td><td>'.$row['firstName'].' '.$row['lastName'].'</td><td>'.$row['year'].'</td></tr>';
+        $i++;
+    }
+
+echo '</table>';
+    }
+
+catch(PDOException $e)
+{
+    $error = "The error during fetching the data from the database ".$e -> getMessage();
+    include 'error.html.php';
+    exit();
 }
